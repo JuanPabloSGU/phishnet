68c5d023a971561111ab5d3fff1df67ac1467b9b
JuanPabloSGU <jsanc016@uottawa.ca>
2024-03-23 18:29:32 -0400

    feat: added basic swagger and restful api to flask

--- server/phishnet/__init__.py
+++ server/phishnet/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_restful import Api
+from flasgger import Swagger
 from .blueprints.endpoints import blueprint as endpoints
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 app.register_blueprint(endpoints)
+
+Api(app)
+Swagger(app)
 
 if __name__ == '__main__':
     app.run()
--- server/phishnet/blueprints/endpoints/__init__.py
+++ server/phishnet/blueprints/endpoints/__init__.py
@@ -1,16 +1,53 @@
 from flask import Blueprint
+from flask_restful import Api, Resource
+from flasgger import swag_from
 from phishnet import elastic
 
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
+api = Api(blueprint)
 
-@blueprint.route('/hello_world')
-def hello_world():
-    return {'message': 'Hello, World!'}
-
-
-@blueprint.route('/elastic')
-def elasticsearch():
-    es = elastic.get_elastic().info()
-    return {'message': 'Hello, ElasticSearch!',
-            'info': es
+class HelloWorldResource(Resource):
+    @swag_from({
+        'responses': {
+            200: {
+                'description': 'Hello, World!',
+                'schema': {
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        }
+                    }
+                }
             }
+        }
+    })
+    def get(self):
+        return {'message': 'Hello, World!'}
+
+api.add_resource(HelloWorldResource, '/hello_world')
+
+class ElasticsearchResource(Resource):
+    @swag_from({
+        'responses': {
+            200: {
+                'description': 'Hello, ElasticSearch!',
+                'schema': {
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                        'info': {
+                            'type': 'object'
+                        }
+                    }
+                }
+            }
+        }
+    })
+    def get(self):
+        es = elastic.get_elastic().info()
+        return {'message': 'Hello, ElasticSearch!', 'info': es}
+
+api.add_resource(ElasticsearchResource, '/elastic')
