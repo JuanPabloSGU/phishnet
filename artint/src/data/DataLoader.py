7dbd8d6391df76814fd2a07033e4fba0787b3652
Alfred Genadri <alfredgenadri@gmail.com>
2024-03-26 19:59:43 -0400

    dataloader

--- artint/src/data/DataLoader.py
+++ artint/src/data/DataLoader.py
@@ -1,4 +1,37 @@
 
 class DataLoader:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, response) -> None:
+        self.response = response
+        self.load_feats()
+
+        self.numeric = self.get_numeric_feats()
+        self.alphabeltical = self.get_alphabetical_feats()
+    
+    def load_feats(self):
+        hits = self.response['data']['hits']['hits']
+        self.feats = list(hits[0]['_source'].keys())
+
+        self.data = []
+        for hit in hits:
+            lst = []
+            for feat in self.feats:
+                lst.append(hit['_source'][feat])
+            self.data.append(lst)
+        return
+    
+    def get_type_feats(self, _types):
+        return [self.feats[idx] for idx, hit in enumerate(self.data[0]) if isinstance(hit, _types)]
+    
+    def get_numeric_feats(self):
+        return self.get_type_feats((int, float))
+
+    def get_alphabetical_feats(self):
+        return self.get_type_feats((str))
+    
+r = {}
+dl = DataLoader(
+    r
+)
+
+print(dl.feats)
+print(dl.data)
