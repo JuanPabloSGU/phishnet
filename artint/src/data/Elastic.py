92a433e889481fca6d67a6c9a733e42d66e2e65c
JuanPabloSGU <jsanc016@uottawa.ca>
2024-03-28 18:34:55 -0400

    feat: elasticsearch class

---
+++ artint/src/data/Elastic.py
@@ -0,0 +1,27 @@
+import os
+from elasticsearch import Elasticsearch
+from dotenv import load_dotenv
+
+class Elastic:
+    def __init__(self, timeout: int) -> Elasticsearch:
+        load_dotenv()
+        self.host = os.getenv('ELASTICSEARCH_HOST')
+        self.user = os.getenv('ELASTICSEARCH_USER')
+        self.password = os.getenv('ELASTICSEARCH_PASSWORD')
+
+        return Elasticsearch(
+            self.host,
+            basic_auth=(self.user, self.password),
+            timeout=timeout
+        )
+
+    def search_entire_index(self, index: str) -> dict:
+        return self.search(
+            index,
+            body={
+                'size': 10000,
+                'query': {
+                    'match_all': {}
+                }
+            }
+        )
