82dd73a07765d5bc267e6fac1898764386916c3a
Alfred Genadri <alfredgenadri@gmail.com>
2024-04-01 15:50:30 -0400

    dataloader + simple log reg complete

--- artint/src/data/DataLoader.py
+++ artint/src/data/DataLoader.py
@@ -1,65 +1,64 @@
-from Elastic import Elastic
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
+import numpy as np
 
 class DataLoader:
-    def __init__(self, response) -> None:
-        self.response = response
-        self.load_feats()
+    def __init__(self, elastic_client, index='featext', scroll='2m', size=10000) -> None:
+        
+        self.elastic_client = elastic_client
 
-        self.numeric = self.get_numeric_feats()
-        self.alphabeltical = self.get_alphabetical_feats()
-    
-    def load_feats(self):
-        hits = self.response['hits']['hits']
-        self.feats = list(hits[0]['_source'].keys())
+        params = {'scroll':scroll, 'index':index, 'body':{'size': size,'query': {'match_all': {}}}}
+        page = self.elastic_client.fetch_index(params)
 
-        self.data = []
-        for hit in hits:
-            lst = []
-            for feat in self.feats:
-                lst.append(hit['_source'][feat])
-            self.data.append(lst)
-        return
-    
-    def to_df(self):
-        self.df = pd.DataFrame(self.data, columns=self.feats)
-    
-    def head_tail(self):
-        return self.df.head(5), self.df.tail(5)
-    
-    def info(self):
-        return self.df.info()
-    
-    def count_duplicates(self):
-        return self.df.groupby(self.df.columns.tolist(),as_index=False).size()
-    
-    def num_null_values(self):
-        return self.df.isnull().sum()
+        df_lst = []
+        df_lst.append(pd.DataFrame.from_dict([document['_source'] for document in page['hits']['hits']]))
 
-    def num_uniques(self):
-        return self.df.nunique()
-    
-    def statistical_analysis(self):
-        return self.df.describe().T
+        if page is None:
+            raise ValueError("Missing Response")
 
-    def get_type_feats(self, _types):
-        return [self.feats[idx] for idx, hit in enumerate(self.data[0]) if isinstance(hit, _types)]
-    
-    def get_numeric_feats(self):
-        return self.get_type_feats((int, float))
+        sid = page['_scroll_id']
+        scroll_size = page['hits']['total']['value']
 
-    def get_alphabetical_feats(self):
-        return self.get_type_feats((str))
-    
-    def train_test_split(self, train_split, random_state): 
-        return train_test_split(self.data, train_size=train_split, random_state=random_state)
-    
-elastic_connection = Elastic(timeout=5)
+        # Start scrolling
+        while (scroll_size > 0):
+            page = self.elastic_client.scroll(scroll_id = sid, scroll = '2m')
+            sid = page['_scroll_id']
+            scroll_size = len(page['hits']['hits'])
 
-dl = DataLoader(
-    elastic_connection.search_entire_index('featext')
-)
+            df_lst.append(pd.DataFrame.from_dict([document['_source'] for document in page['hits']['hits']]))
+        
+        self.df = pd.concat(df_lst)
+        self.df.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+        
+        self.cast_astype()
+        self.lex_df = self.extract_lexical()
 
-dl.to_df()
+    def __len__(self):
+        return len(self.df)
+    
+    def get_feats(self):
+        return list(self.df.columns)
+    
+    def is_float(x):
+        try:
+            float(x)
+            return True
+        except ValueError:
+            return False
+    
+    def cast_astype(self):
+        for col in self.df.select_dtypes(include='object').columns:
+            if self.df[col].apply(lambda x: str(x).isdigit()).all():
+                self.df[col] = self.df[col].astype(int)
+            elif self.df[col].apply(lambda x: DataLoader.is_float(x)).all():
+                self.df[col] = self.df[col].astype(float)
+    
+    # refactor 
+    def extract_lexical(self):
+        lexical_feats = ['len_url', 'len_component', 'count_digits_component', 'count_letters_component', 'ratio_digits_component_url', 'ratio_letters_component_url', 'count_dots_url', 'count_percent_url', 'count_hash_url', 'count_ats_url', 'count_embed_url', 'use_https', 'no_of_directories', 'contains_ip_address', 'character_continuity_rate_url', 'shannon_entropy_url']
+        lexical_feats.append('type')
+        return self.df[lexical_feats]
+
+    def train_test_split(self, X, y, train_split, random_state): 
+        return train_test_split(X, y, train_size=train_split, random_state=random_state)
--- artint/src/data/Elastic.py
+++ artint/src/data/Elastic.py
@@ -1,27 +1,30 @@
 import os
 from elasticsearch import Elasticsearch
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 class Elastic:
     def __init__(self, timeout: int) -> Elasticsearch:
-        load_dotenv()
+        
+        #load_dotenv()
         self.host = os.getenv('ELASTICSEARCH_HOST')
         self.user = os.getenv('ELASTICSEARCH_USER')
         self.password = os.getenv('ELASTICSEARCH_PASSWORD')
+
         self.connection = Elasticsearch(
             self.host,
             basic_auth=(self.user, self.password),
             timeout=timeout
         )
 
-    def search_entire_index(self, index: str) -> dict:
+    def fetch_index(self, params) -> dict:
         return self.connection.search(
             http_auth=(self.user, self.password),
-            index=index,
-            body={
-                'size': 10000,
-                'query': {
-                    'match_all': {}
-                }
-            }
+            **params
+        )
+
+    def scroll(self, scroll_id, scroll = '2m'):
+        return self.connection.scroll(
+            http_auth=(self.user, self.password),
+            scroll_id = scroll_id, 
+            scroll = scroll
         )
