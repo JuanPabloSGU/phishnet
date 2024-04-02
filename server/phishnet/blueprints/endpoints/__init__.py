82392504909cc49b4146bada67779ac86f966392
JuanPabloSGU <jsanc016@uottawa.ca>
2024-04-01 23:46:36 -0400

    feat: basic query to triton, now working :)

--- server/phishnet/blueprints/endpoints/__init__.py
+++ server/phishnet/blueprints/endpoints/__init__.py
@@ -1,5 +1,8 @@
+import json
 import os
+import requests
 import base64
+import numpy as np
 from flask import Blueprint
 from flask_restful import Api, Resource, reqparse
 from flasgger import swag_from
@@ -53,41 +56,38 @@ class ElasticsearchResource(Resource):
         es = elastic.get_elastic().info(pretty=True)
         return {'message': 'Hello, ElasticSearch!', 'info': es.body}
 
-api.add_resource(ElasticsearchResource, '/elastic')
 
-class TestElasticsearchResource(Resource):
-    def get(self):
-        es = elastic.get_elastic()
-        idx = 'raw'
-        data = es.search(index=idx, body={'query': {'match_all': {}}}, size=100)
-        return {'message': 'Test Elasticsearch!', 'data': data.body}
-
-    def post(self):
-        es = elastic.get_elastic()
-        idx = 'test-data'
-        es.create(index=idx, id=1, body={'test': 'data'})
-        return {'message': 'Test Elasticsearch!'}
-
-api.add_resource(TestElasticsearchResource, '/test_elastic')
-
-class LexicalFeatureResource(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('url', type=str, required=True, help='URL to extract lexical features from.')
-        args = parser.parse_args()
-
-        url = args['url']
-        if url is None:
-            return {'message': 'No URL provided.'}
-        
-        es = elastic.get_elastic()
-        res = es.search(index='featext', body={'query': {'match': {'url': url}}})
-
-        return {'message': 'Lexical Features extracted.',
-                'url': url,
-                'data': res.body
+class LogisticalRegression(Resource):
+    @swag_from({
+        'parameters': [
+            {
+                'name': 'url',
+                'description': 'URL to extract lexical features from.',
+                'in': 'formData',
+                'type': 'string',
+                'required': True
+            }
+        ],  
+        'responses': {
+            200: {
+                'description': 'Lexical Features extracted and stored.',
+                'schema': {
+                    'type': 'object',
+                    'properties': {
+                        'message': {
+                            'type': 'string'
+                        },
+                        'url': {
+                            'type': 'string'
+                        },
+                        'data': {
+                            'type': 'object'
+                        }
+                    }
                 }
-
+            }
+        }
+    })
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('url', type=str, required=True, help='URL to extract lexical features from.')
@@ -99,26 +99,56 @@ class LexicalFeatureResource(Resource):
 
         es = elastic.get_elastic()
         idx_raw = 'raw'
-        idx_feat = 'featext'
-
-        if url is None:
-            return {'message': 'No URL provided.'}
+        idx_feat = 'test_feat'
 
         # Run Lexical Feature Extraction
         # Step 1 - Check if URL exists in Elasticsearch
-        res = es.search(index=idx_raw, body={'query': {'match': {'url': url}}})
-        if res['hits']['total'] == 1:
+        res = es.search(
+            index=idx_raw,
+            body={
+                'query': {
+                    'term': {
+                        'url.keyword': url
+                    }
+                }
+            }
+        )
+        if res['hits']['total']['value'] > 0:
             return {'message': 'URL found in raw index, skipping feature extraction.'}
 
         # Step 2 - Extract Lexical Features
-        lexical = Lexical([url])
-        lexical.extract()
-        data = lexical.feat_dict
+        lexical = Lexical()
+        lexical.extract(url)
+        features = lexical.feat_dict
 
         # Step 3 - Store Lexical Features in Elasticsearch
-        es.create(index=idx_feat, id=base64.b64encode(url.encode()).decode(), body=data)
+        es.index(index=idx_feat, body=features)
+
+        data = np.array(list(features.values())[1:]).astype(np.float32).tolist()
+
+        payload = {
+            "inputs": [
+                {
+                    "name": "input",
+                    "shape": [1, len(data)],
+                    "datatype": "FP32",
+                    "data": data
+                }
+            ]
+        }
+
+        triton_server_url = "https://triton.capstone.databending.ca"
+        model_name = "logisticalRegression"
+        inference_url = f"{triton_server_url}/v2/models/{model_name}/infer"
+
+        res = requests.post(inference_url, 
+                            data=json.dumps(payload),
+                            headers={'Content-Type': 'application/json'}
+                            )
 
         return {'message': 'Lexical Features extracted and stored.',
-                'url': url}
+                'url': url,
+                'data': features,
+                'triton': res.json()}
 
-api.add_resource(LexicalFeatureResource, '/lexical')
+api.add_resource(LogisticalRegression, '/logres')
--- server/phishnet/blueprints/features/Lexical.py
+++ server/phishnet/blueprints/features/Lexical.py
@@ -8,68 +8,79 @@ from ipaddress import ip_address
 
 class Lexical: 
 
-    def __init__(self, urls: list) -> None:
-        self.urls = urls
+    def __init__(self) -> None:
         self.feat_dict = {}
 
-    def extract(self) -> list: 
+    def extract(self, url): 
+        scheme, netloc, path, params, query, fragment = urlparse(url)
 
-        for url in self.urls:
-            scheme, netloc, path, params, query, fragment = urlparse(url)
+        self.feat_dict['url'] = url
+        self.feat_dict['lexical_len_url'] = len(url)
 
-            self.feat_dict[f'url'] = url
-            self.feat_dict['len_url'] = len(url)
+        for name, component in {'netloc': netloc, 'path': path}.items():
+            self.feat_dict[f'lexical_len_{name}'] = len(component)
+            self.feat_dict[f'lexical_count_digits_{name}'] = Lexical.count_digits(component)
+            self.feat_dict[f'lexical_count_letters_{name}'] = Lexical.count_letters(component)
+            self.feat_dict[f'lexical_ratio_digits_{name}_url'] = Lexical.component_ratio(self.feat_dict[f'lexical_count_digits_{name}'], url)
+            self.feat_dict[f'lexical_ratio_letters_{name}_url'] = Lexical.component_ratio(self.feat_dict[f'lexical_count_letters_{name}'], url)
 
-            for component in [netloc, path]:
-                name = f'{component=}'.partition('=')[0]
+        self.feat_dict['lexical_count_dots_url'] = Lexical.count_sub(url, '.')
+        self.feat_dict['lexical_count_percent_url'] = Lexical.count_sub(url, '%')
+        self.feat_dict['lexical_count_hash_url'] = Lexical.count_sub(url, '#')
+        self.feat_dict['lexical_count_ats_url'] = Lexical.count_sub(url, '@')
+        self.feat_dict['lexical_count_embed_url'] = Lexical.count_sub(url, '//')
 
-                self.feat_dict[f'len_{name}'] = len(component)
-                self.feat_dict[f'count_digits_{name}'] = Lexical.count_digits(component)
-                self.feat_dict[f'count_letters_{name}'] = Lexical.count_letters(component)
-                self.feat_dict[f'ratio_digits_{name}_url'] = Lexical.component_ratio(self.feat_dict[f'count_digits_{name}'], url)
-                self.feat_dict[f'ratio_letters_{name}_url'] = Lexical.component_ratio(self.feat_dict[f'count_letters_{name}'], url)
-        
-            self.feat_dict['count_dots_url'] = Lexical.count_sub(url, '.')
-            self.feat_dict['count_percent_url'] = Lexical.count_sub(url, '%')
-            self.feat_dict['count_hash_url'] = Lexical.count_sub(url, '#')
-            self.feat_dict['count_ats_url'] = Lexical.count_sub(url, '@')
-            self.feat_dict['count_embed_url'] = Lexical.count_sub(url, '//')
+        self.feat_dict['lexical_use_https'] = Lexical.uses_https(scheme)
+        self.feat_dict['lexical_no_of_directories'] = Lexical.no_of_directories(path)
+        self.feat_dict['lexical_contains_ip_address'] = Lexical.contains_ip_address(netloc)
+        self.feat_dict['lexical_character_continuity_rate_url'] = Lexical.character_continuity_rate(url)
 
-            self.feat_dict['use_https'] = Lexical.uses_https(scheme)
-            self.feat_dict['no_of_directories'] = Lexical.no_of_directories(path)
-            self.feat_dict['contains_ip_address'] = Lexical.contains_ip_address(netloc)
-            self.feat_dict['character_continuity_rate_url'] = Lexical.character_continuity_rate(url)
+        self.feat_dict['lexical_shannon_entropy_url'] = Lexical.shannon_entropy(url)
 
-            self.feat_dict['shannon_entropy_url'] = Lexical.shannon_entropy(url)
-
+        return self.feat_dict
 
     @staticmethod
     def component_ratio(one, two):
-        p = len(one) if type(one) is str else one
-        q = len(two) if type(two) is str else two
+        try:
+            p = len(one) if type(one) is str else one
+            q = len(two) if type(two) is str else two
 
-        if not(p and q):
-            return 0
-        return p / q
+            if not(p and q):
+                return 0
+            return p / q
+        except:
+            return None
 
     @staticmethod
     def count_digits(_str: str):
-        return sum(c.isdigit() for c in _str)
+        try:
+            return sum(c.isdigit() for c in _str)
+        except:
+            return None
 
     @staticmethod
     def count_letters(_str: str):
-        return sum(c.isalpha() for c in _str)
+        try:
+            return sum(c.isalpha() for c in _str)
+        except:
+            return None
     
     @staticmethod
     def uses_https(scheme: str):
         """Check if a given URL scheme is HTTPS."""
-        return scheme == 'https'
+        try:
+            return 1 if scheme == 'https' else 0
+        except:
+            return None
 
     @staticmethod
     def shannon_entropy(url: str):
         """Calculate the Shannon entropy of a URL. Used to catch URLs with high randomness."""
-        prob = [float(url.count(c)) / len(url) for c in dict.fromkeys(list(url))]
-        return - sum([p * log2(p) for p in prob])
+        try:
+            prob = [float(url.count(c)) / len(url) for c in dict.fromkeys(list(url))]
+            return - sum([p * log2(p) for p in prob])
+        except:
+            return None
     
     @staticmethod
     def relative_entropy(url: str):
@@ -79,22 +90,31 @@ class Lexical:
     @staticmethod
     def alphabet_entropy(netloc):
         """Calculate the entropy of the domain based on its alphabetic characters."""
+        try:
         # Extract the domain name and focus only on alphabetic characters
-        domain = netloc.split(':')[0]  # Remove port number if present
-        alphabet = re.sub(r'[^a-zA-Z]', '', domain)  # Keep only alphabetic characters
-        alphabet_freq = Counter(alphabet) # Frequency of each alphabetic character
-        total_chars = len(alphabet)
-        return -sum((count / total_chars) * log2(count / total_chars) for count in alphabet_freq.values() if count > 0)
-    
+            domain = netloc.split(':')[0]  # Remove port number if present
+            alphabet = re.sub(r'[^a-zA-Z]', '', domain)  # Keep only alphabetic characters
+            alphabet_freq = Counter(alphabet) # Frequency of each alphabetic character
+            total_chars = len(alphabet)
+            return -sum((count / total_chars) * log2(count / total_chars) for count in alphabet_freq.values() if count > 0)
+        except:
+            return None
+
     @staticmethod
     def count_sub(_str: str, _sub: str):
         """Count occurences of substring in string"""
-        return _str.count(_sub)
+        try:
+            return _str.count(_sub)
+        except:
+            return None
     
     @staticmethod
     def no_of_directories(path: str):
         """Count the number of directories in the URL path."""
-        return len(path.split('/')) - 1
+        try:
+            return len(path.split('/')) - 1
+        except:
+            return None
     
     @staticmethod
     def contains_ip_address(netloc: str):
@@ -111,7 +131,18 @@ class Lexical:
     @staticmethod
     def character_continuity_rate(url: str):
         """Calculate the Character Continuity Rate (CCR) of a URL."""
-        consecutive_chars = re.findall(r'(.)\1+', url) # Find all consecutive characters
-        total_consecutive_length = sum(len(match) for match in consecutive_chars) # Count the total length of all consecutive characters
-        return 0 if len(url) == 0 else total_consecutive_length / len(url)
-    
+        try:
+            consecutive_chars = re.findall(r'(.)\1+', url) # Find all consecutive characters
+            total_consecutive_length = sum(len(match) for match in consecutive_chars) # Count the total length of all consecutive characters
+            return 0 if len(url) == 0 else total_consecutive_length / len(url)
+        except:
+            return None
+
+
+# example = Lexical()
+# print(example.extract('https://www.google.com'))
+
+# for keys in example.feat_dict:
+#     print(keys + " " + str(type(example.feat_dict[keys])))
+#     print(example.feat_dict[keys])
+#     print()
