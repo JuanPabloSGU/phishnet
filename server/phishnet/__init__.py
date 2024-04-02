e04105d29c42a3479090315da7c32463652bfc00
JuanPabloSGU <jsanc016@uottawa.ca>
2024-04-01 22:30:00 -0400

    feat: added basic tests and factory method of creating app

--- server/phishnet/__init__.py
+++ server/phishnet/__init__.py
@@ -4,12 +4,16 @@ from flasgger import Swagger
 from .blueprints.endpoints import blueprint as endpoints
 
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')
-app.register_blueprint(endpoints)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    app.register_blueprint(endpoints)
 
-Api(app)
-Swagger(app)
+    Api(app)
+    Swagger(app)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run()
