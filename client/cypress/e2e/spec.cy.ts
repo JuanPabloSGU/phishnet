c42ca24c483a8c9eb233f5db9ae1f6f8ffb7c5e8
JuanPabloSGU <jsanc016@uottawa.ca>
2024-09-16 12:05:41 -0400

    feat: add more basic test cases

--- client/cypress/e2e/spec.cy.ts
+++ client/cypress/e2e/spec.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
+describe('visit home page', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:5173')
   })
+})
+
+describe('visit about page', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:5173/about')
+    })
+})
+
+describe('visit contact page', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:5173/contact')
+    })
+})
+
+describe('visit login page', () => {
+    it('passes', () => {
+        cy.visit('http://localhost:5173/login')
+    })
 })
