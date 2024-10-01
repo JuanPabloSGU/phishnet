d7579306f6379f73ffd0c1459e6da6b4ffacf445
Adam Jasniewicz <adam@databending.ca>
2024-09-30 20:32:45 -0400

    zitadel auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,20 @@
-import '@mantine/core/styles.css'
-import { HeaderSimple } from './components/HeaderSimple'
-import { FeaturesTitle } from './components/FeaturesTitle'
-import { Inference } from './components/Inference'
-import { PreviousSearches } from './components/PreviousSearches'
-import { FooterSimple } from './components/FooterSimple'
+import "@mantine/core/styles.css";
+import { HeaderSimple } from "./components/HeaderSimple";
+import { FeaturesTitle } from "./components/FeaturesTitle";
+import { Inference } from "./components/Inference";
+import { PreviousSearches } from "./components/PreviousSearches";
+import { FooterSimple } from "./components/FooterSimple";
 
 function App() {
-
-    return (
-        <>
-            <HeaderSimple />
-            <FeaturesTitle />
-            <Inference />
-            <PreviousSearches />
-            <FooterSimple />
-        </>
-    )
+  return (
+    <>
+      <HeaderSimple />
+      <FeaturesTitle />
+      <Inference />
+      <PreviousSearches />
+      <FooterSimple />
+    </>
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,19 +1,37 @@
-import '@mantine/core/styles.css'
-import { Authentication } from './components/Authentication'
-import { HeaderSimple } from '../../components/HeaderSimple'
-import { Container } from '@mantine/core'
-import { FooterSimple } from '../../components/FooterSimple'
+import "@mantine/core/styles.css";
+import { Authentication } from "./components/Authentication";
+import { HeaderSimple } from "../../components/HeaderSimple";
+import { Container } from "@mantine/core";
+import { FooterSimple } from "../../components/FooterSimple";
+import { createZitadelAuth, ZitadelConfig } from "@zitadel/react";
+import { useEffect, useState } from "react";
 
 function Login() {
-    return (
-        <>
-            <HeaderSimple />
-            <Container>
-                <Authentication />
-            </Container>
-            <FooterSimple />
-        </>
-    )
+  const config: ZitadelConfig = {
+    authority: "https://zitadel.databending.ca",
+    client_id: "287272511991840275",
+  };
+
+  const zitadel = createZitadelAuth(config);
+
+  function login() {
+    zitadel.authorize();
+  }
+
+  function signout() {
+    zitadel.signout();
+  }
+
+
+  return (
+    <>
+      <HeaderSimple />
+      <Container>
+        <Authentication  />
+      </Container>
+      <FooterSimple />
+    </>
+  );
 }
 
-export default Login
+export default Login;
