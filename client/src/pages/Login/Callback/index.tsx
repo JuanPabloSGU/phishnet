3a3606c5a3e806bdbc6441c54124bcc1f8c842f2
Adam Jasniewicz <adam@databending.ca>
2024-11-29 12:35:05 -0500

    fix: profile

--- client/src/pages/Login/Callback/index.tsx
+++ client/src/pages/Login/Callback/index.tsx
@@ -1,27 +1,29 @@
 import { useAuth } from "@context/Auth";
-
+import { ActionIcon, Button, Flex, Text, TextInput } from "@mantine/core";
+import { IconCheck } from "@tabler/icons-react";
 const Callback = () => {
-  const { authenticated,  userInfo, signout } = useAuth();
-
+  const { authenticated, userInfo, signout } = useAuth();
 
   if (authenticated === true && userInfo) {
     return (
       <div className="user">
         <h2>Welcome, {userInfo.profile.name}!</h2>
-        <p className="description">Your ZITADEL Profile Information</p>
-        <p>Name: {userInfo.profile.name}</p>
-        <p>Email: {userInfo.profile.email}</p>
-        <p>Email Verified: {userInfo.profile.email_verified ? "Yes" : "No"}</p>
-        <p>
-          Roles: {JSON.stringify(
-            userInfo.profile[
-            "urn:zitadel:iam:org:project:roles"
-            ],
-          )}
-        </p>
+        <TextInput disabled label="Name" value={userInfo.profile.name} />
+        <TextInput disabled label="Email" value={userInfo.profile.email} />
+        <Flex mt={4} mb="lg" align="center">
+          <Text size="sm" mr={5}>Email Verified</Text>
+          <ActionIcon
+            variant="filled"
+            size="sm"
+            radius="xl"
+            color="green"
+            aria-label="Settings"
+          >
+            <IconCheck style={{ width: "90%", height: "90%" }} stroke={2} />
+          </ActionIcon>
+        </Flex>
 
-        <p>JWT: {userInfo.id_token}</p>
-        <button onClick={signout}>Log out</button>
+        <Button onClick={signout}>Log out</Button>
       </div>
     );
   } else {
